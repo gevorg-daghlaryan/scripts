60d195bc6da2e7b0e2f73584255c5917dde7c0d1
Gevorg Daghlaryan <gevorg.daghlaryan@esterox.am>
2023-05-26 16:30:12 +0400

    Bubble Sorting

--- script.js
+++ script.js
@@ -11,9 +11,29 @@ let ladder = {
     }
 };
 
-ladder.up();
-ladder.up();
-ladder.down();
-ladder.showStep();
-ladder.down();
-ladder.showStep();
+// ladder.up();
+// ladder.up();
+// ladder.down();
+// ladder.showStep();
+// ladder.down();
+// ladder.showStep();
+
+
+//Bubble Sort
+
+function bubbleSort(arr){
+
+    for(let i = 0; i < arr.length; i++){
+
+        for(let j = 0; j < arr.length - i - 1; j++){
+
+            if(arr[j + 1] < arr[j]){
+
+                [arr[j + 1],arr[j]] = [arr[j],arr[j + 1]]
+            }
+        }
+    };
+    return arr;
+};
+
+console.log(bubbleSort([5,3,8,4,6,-1]));
