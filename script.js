abe8d69e8d70088094c39ff6ed2707e05af01ee5
Gevorg Daghlaryan <gevorg.daghlaryan@esterox.am>
2023-06-02 17:32:48 +0400

    Insertion Sort

--- script.js
+++ script.js
@@ -37,3 +37,25 @@ function bubbleSort(arr){
 };
 
 console.log(bubbleSort([5,3,8,4,6,-1]));
+
+function insertionSort(arr){
+    //Start from the second element.
+    for(let i = 1; i < arr.length;i++){
+
+        //Go through the elements behind it.
+        for(let j = i - 1; j > -1; j--){
+
+            //value comparison using ascending order.
+            if(arr[j + 1] < arr[j]){
+
+                //swap
+                [arr[j+1],arr[j]] = [arr[j],arr[j + 1]];
+
+            }
+        }
+    };
+
+    return arr;
+}
+
+console.log(insertionSort([23, 1, 10, 5, 2]));
