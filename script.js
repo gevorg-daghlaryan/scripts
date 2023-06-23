3ef2ded80510e0362084af6c7c73521acdc23282
Gevorg Daghlaryan <gevorg.daghlaryan@esterox.am>
2023-06-23 17:39:01 +0400

    added heapsort

--- script.js
+++ script.js
@@ -96,3 +96,32 @@ function LinearSearch(t,A)
         if (i < n) return i;
         else return -1;
     }
+
+
+
+    const heapsort = arr => {
+    const a = [...arr];
+    let l = a.length;
+
+    const heapify = (a, i) => {
+        const left = 2 * i + 1;
+        const right = 2 * i + 2;
+        let max = i;
+        if (left < l && a[left] > a[max]) max = left;
+        if (right < l && a[right] > a[max]) max = right;
+        if (max !== i) {
+            [a[max], a[i]] = [a[i], a[max]];
+            heapify(a, max);
+        }
+    };
+
+    for (let i = Math.floor(l / 2); i >= 0; i -= 1) heapify(a, i);
+    for (i = a.length - 1; i > 0; i--) {
+        [a[0], a[i]] = [a[i], a[0]];
+        l--;
+        heapify(a, 0);
+    }
+    return a;
+};
+
+heapsort([6, 3, 4, 1]);
