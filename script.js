909f5cfd2b2fa1c6c6c4c37b5acd290ee73363ca
Gevorg Daghlaryan <gevorg.daghlaryan@esterox.am>
2023-07-21 17:26:41 +0400

    added permutatioms

--- script.js
+++ script.js
@@ -171,3 +171,16 @@ const radixSort = nums => {
     return nums
 }
 console.log(radixSort(arr)) // [1,2,3,4,5,6,7,8,9,10]
+const permutations = arr => {
+    if (arr.length <= 2) return arr.length === 2 ? [arr, [arr[1], arr[0]]] : arr;
+    return arr.reduce(
+        (acc, item, i) =>
+            acc.concat(
+                permutations([...arr.slice(0, i), ...arr.slice(i + 1)]).map(val => [
+                    item,
+                    ...val,
+                ])
+            ),
+        []
+    );
+};
