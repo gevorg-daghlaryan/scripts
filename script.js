d465a4da184eccac242941939e83630bce82acae
Gevorg Daghlaryan <gevorg.daghlaryan@esterox.am>
2023-07-28 16:13:44 +0400

    add LPS algorithm

--- script.js
+++ script.js
@@ -184,3 +184,24 @@ const permutations = arr => {
         []
     );
 };
+
+function calculateLpsTable(subStr) {
+    let i = 1;
+    let j = 0;
+    let lps = new Array(subStr.length).fill(0);
+
+    while(i < subStr.length) {
+        if(subStr[i] === subStr[j]) {
+            lps[i] = j + 1;
+            i += 1;
+            j += 1;
+        } else {
+            if(j !== 0) {
+                j = lps[j - 1];
+            } else {
+                i += 1;
+            }
+        }
+    }
+    return lps;
+}
