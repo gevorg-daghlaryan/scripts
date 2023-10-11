db0b83abddeb2d62f0429abd8deb43deb602f349
Gevorg Daghlaryan <gevorg.daghlaryan@esterox.am>
2023-10-11 09:40:21 +0400

    added bubble sort

--- script.js
+++ script.js
@@ -205,3 +205,20 @@ function calculateLpsTable(subStr) {
     }
     return lps;
 }
+
+function bubbleSort(arr){
+
+    for(let i = 0; i < arr.length; i++){
+
+        for(let j = 0; j < arr.length - i - 1; j++){
+
+            if(arr[j + 1] < arr[j]){
+
+                [arr[j + 1],arr[j]] = [arr[j],arr[j + 1]]
+            }
+        }
+    };
+    return arr;
+};
+
+console.log(bubbleSort([5,3,8,4,6,-1]));
